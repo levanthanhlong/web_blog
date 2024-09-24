9ea4683cd0f65e2eb8f120add4a007707a00a9fc
levanthanhlong <thanhtdbn132001@gmail.com>
2024-09-24 16:28:59 +0700

    first commit

--- src/postdb.js
+++ src/postdb.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 async function main() {
-  // mongoose.connect("mongod://localhost:27017/todolistDb", {
-  //   useNewUrlParser: true,
-  // });
-  await mongoose.connect(
-    "mongodb+srv://arjuncvinod:gdozFKJP7i12I87s@cluster0.yjxy0xp.mongodb.net/todoListDB",
-    { useNewUrlParser: true }
-  );
+  mongoose.connect("mongodb://localhost:27017/todolistDb", {
+    useNewUrlParser: true,
+  });
+  // await mongoose.connect(
+  //   "mongodb+srv://arjuncvinod:gdozFKJP7i12I87s@cluster0.yjxy0xp.mongodb.net/todoListDB",
+  //   { useNewUrlParser: true }
+  // );
   // mongoose.connect("mongodb://127.0.0.1:27017/myblog") for local DB
-  console.log("post connected");
+  console.log("connected mongodb");
 }
 main()
 const postSchema = new mongoose.Schema({
--- src/profiledb.js
+++ src/profiledb.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 async function main() {
-  // mongoose.connect("mongod://localhost:27017/todolistDb", {
-  //   useNewUrlParser: true,
-  // });
-  await mongoose.connect(
-    "mongodb+srv://arjuncvinod:gdozFKJP7i12I87s@cluster0.yjxy0xp.mongodb.net/todoListDB",
-    { useNewUrlParser: true }
+  mongoose.connect("mongodb://localhost:27017/todolistDb", {
+    useNewUrlParser: true,
+  });
+  // await mongoose.connect(
+  //   "mongodb+srv://arjuncvinod:gdozFKJP7i12I87s@cluster0.yjxy0xp.mongodb.net/todoListDB",
+  //   { useNewUrlParser: true }
     
-  );
+  // );
   // mongoose.connect("mongodb://127.0.0.1:27017/myblog") for local Db
-  console.log("profile connected");
+  console.log("connected profile db");
 }
 main();
 const profileSchema = new mongoose.Schema({
